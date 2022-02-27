264a0d6accb34e1c3517c0332994e9fcecb947fb
Sanjeev <94648747+CurryBoy-Knows-To-Code@users.noreply.github.com>
2022-02-27 19:34:31 +0000

    Add files via upload

--- My School Work/The 20 Coding Challenges/Count Words In a String Problem/the_code.py
+++ My School Work/The 20 Coding Challenges/Count Words In a String Problem/the_code.py
@@ -1,1 +1,9 @@
+# this is just a neat way of writing 'datafile = open(file,"r")'
+with open("file with string.txt", "r") as datafile:
+    number_of_word = 0
+    for word in datafile:
+        # the empty split remove spaces an just splits them
+        number_of_word += (len(word.split()))
 
+
+print(number_of_word)
